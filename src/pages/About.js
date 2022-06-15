24526e26119c03c6b95fdc7efca779695d617fa7
Charlie J <cjin@MacBook-Pro-5.local>
2022-06-14 22:17:09 -0700

    week1-week2-changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import "./About.css";
+import ShibaInu from "../assets/ShibaInu.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              // Image goes here
+              src={ShibaInu}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Charlie Jin</div>
+            <div className="brief_description">
+              CP4BAaaS development manager at IBM
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
